5ceba93193ab1f7d5a9d998312e7361f90d82ba6
Seantheman01 <sean.v.g@outlook.com>
2022-11-17 14:09:11 +0100

    committed

--- module_03/deel_02/coins.py
+++ module_03/deel_02/coins.py
@@ -9,7 +9,7 @@ paid = int(float(input('Paid amount: ')) * 100) #
 change = paid - toPay #
 
 if change > 0: #
-  coinValue = 50 #
+  coinValue = 500 #
   
   while change > 0 and coinValue > 0: #
     nrCoins = change // coinValue #

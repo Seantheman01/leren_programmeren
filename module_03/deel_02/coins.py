81a9bfbf87d3fd98f3ebb3583e2253a7e3889962
Seantheman01 <sean.v.g@outlook.com>
2022-11-18 14:58:02 +0100

    committed

--- module_03/deel_02/coins.py
+++ module_03/deel_02/coins.py
@@ -42,9 +42,6 @@ if change > 0: #
 
 if change > 0: # this tells you that if the change is bigger than 0, it will print the underneath
   print('Change not returned: ', str(change) + ' cents')
-  
-  while change > 0:
-      aantal = ' '
     
 else:
   print('done')

604fac6dc751d3d94ac273d63cff6c5273c2d0dd
Seantheman01 <sean.v.g@outlook.com>
2022-11-21 18:37:51 +0100

    committed

--- module_03/deel_02/robot_arm_advanced/robotarm_opdracht9.py
+++ module_03/deel_02/robot_arm_advanced/robotarm_opdracht9.py
@@ -9,33 +9,7 @@ robotArm. grab()
 for x in range(6):
     robotArm.moveRight()
 robotArm.drop()
-for y in range(5):
-    robotArm.moveLeft()
-robotArm.grab()
-for z in range(6):
-    robotArm.moveRight()
-robotArm.drop()
-for a in range(6):
-    robotArm.moveLeft()
-robotArm.grab()
-for b in range(6):
-    robotArm.moveRight()
-robotArm.drop()
-for c in range(5):
-    robotArm.moveLeft()
-robotArm.grab()
-for d in range(6):
-    robotArm.moveRight()
-robotArm.drop()
-for e in range(6):
-    robotArm.moveLeft()
-robotArm.grab()
-for f in range(6):
-    robotArm.moveRight()
-robotArm.drop()
-for g in range(6):
-    robotArm.moveLeft()
-robotArm.grab()
+
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()

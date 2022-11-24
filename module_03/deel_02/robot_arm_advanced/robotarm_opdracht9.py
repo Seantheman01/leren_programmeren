84fd83c9f42d30eb41bf8b475cd5068275b9b9b5
Seantheman01 <sean.v.g@outlook.com>
2022-11-24 09:10:01 +0100

    committed

--- module_03/deel_02/robot_arm_advanced/robotarm_opdracht9.py
+++ module_03/deel_02/robot_arm_advanced/robotarm_opdracht9.py
@@ -4,12 +4,9 @@ robotArm = RobotArm('exercise 9')
 
 # Jouw python instructies zet je vanaf hier:
 robotArm.speed = 3
-
-robotArm. grab()
-for x in range(6):
-    robotArm.moveRight()
-robotArm.drop()
-
+a = 0
+while a < 10:
+    robotArm.grab()
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()

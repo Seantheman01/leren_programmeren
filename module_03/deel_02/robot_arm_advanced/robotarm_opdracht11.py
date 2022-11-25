035e8f84b6b053cdf19414ecf92e9f9743cc58b2
Seantheman01 <sean.v.g@outlook.com>
2022-11-25 09:39:26 +0100

    committed

--- module_03/deel_02/robot_arm_advanced/robotarm_opdracht11.py
+++ module_03/deel_02/robot_arm_advanced/robotarm_opdracht11.py
@@ -5,11 +5,11 @@ robotArm = RobotArm('exercise 11')
 # Jouw python instructies zet je vanaf hier:
 for x in range(9):
     robotArm.grab()
-    robotArm.scan()
+    color = robotArm.scan()
     robotArm.drop()
     robotArm.moveRight()
-if robotArm._color == 'White':
-    robotArm.moveRight()
-    robotArm.drop()   
+    if color == 'white':
+        robotArm.moveRight()
+        robotArm.drop()   
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()

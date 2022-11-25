d15939496a5cc90ea116ea1eb13b8da043722c73
Seantheman01 <sean.v.g@outlook.com>
2022-11-25 13:34:45 +0100

    committed

--- module_03/deel_02/robot_arm_advanced/robotarm_opdracht12.py
+++ module_03/deel_02/robot_arm_advanced/robotarm_opdracht12.py
@@ -4,6 +4,7 @@ robotArm = RobotArm('exercise 12')
 
 # Jouw python instructies zet je vanaf hier:
 robotArm.speed = 3
+a = 10
 for x in range(8):
     robotArm.moveRight()
 for y in range(9):
@@ -11,11 +12,9 @@ for y in range(9):
     kleur = robotArm.scan()
     if kleur == 'red':
         robotArm.moveRight()
+    else:
         robotArm.drop()
         robotArm.moveLeft()
-        robotArm.moveLeft()
-    robotArm.drop()
-    robotArm.moveLeft()
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()

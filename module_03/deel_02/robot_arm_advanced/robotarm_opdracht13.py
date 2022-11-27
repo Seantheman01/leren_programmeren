7daa5120b0e091bf80eb2b1dd8a6553aad1c18fd
Seantheman01 <sean.v.g@outlook.com>
2022-11-27 12:40:25 +0100

    commit

---
+++ module_03/deel_02/robot_arm_advanced/robotarm_opdracht13.py
@@ -0,0 +1,10 @@
+from RobotArm import RobotArm
+# Let op: hier start het anders voor een random level:
+robotArm = RobotArm()
+robotArm.randomLevel(1,7)
+
+# Jouw python instructies zet je vanaf hier:
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()

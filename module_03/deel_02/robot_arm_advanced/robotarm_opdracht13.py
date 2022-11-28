64448d7969b07993c55b79f9850fc05318dbd3ae
Seantheman01 <sean.v.g@outlook.com>
2022-11-28 08:59:02 +0100

    committed

--- module_03/deel_02/robot_arm_advanced/robotarm_opdracht13.py
+++ module_03/deel_02/robot_arm_advanced/robotarm_opdracht13.py
@@ -5,8 +5,7 @@ robotArm.randomLevel(1,7)
 
 # Jouw python instructies zet je vanaf hier:
 robotArm.speed = 3
-for x in range():
-    robotArm.moveRight()
+
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()

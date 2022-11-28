de013a443155d3e1e2da1c7d5c468d242f14a9fe
Seantheman01 <sean.v.g@outlook.com>
2022-11-28 14:56:53 +0100

    committed

--- module_03/deel_02/robot_arm_advanced/robotarm_opdracht12.py
+++ module_03/deel_02/robot_arm_advanced/robotarm_opdracht12.py
@@ -5,7 +5,7 @@ robotArm = RobotArm('exercise 12')
 # Jouw python instructies zet je vanaf hier:
 robotArm.speed = 3
 a = 10
-for y in range(10):
+for y in range(9):
     a = a - 1
     robotArm.grab()
     kleur = robotArm.scan()

9829de3112a5f18541f00ee88372a33368642c1b
Seantheman01 <sean.v.g@outlook.com>
2022-11-28 09:06:25 +0100

    committed

--- module_03/deel_02/robot_arm_advanced/robotarm_opdracht13.py
+++ module_03/deel_02/robot_arm_advanced/robotarm_opdracht13.py
@@ -5,7 +5,13 @@ robotArm.randomLevel(1,7)
 
 # Jouw python instructies zet je vanaf hier:
 robotArm.speed = 3
-
+x = 0
+while x > 0:
+    robotArm.grab()
+    kleur = robotArm.scan()
+    if kleur == 'white' or kleur == 'red' or kleur == 'blue' or kleur == 'green' or kleur == 'yellow':
+        robotArm.moveRight()
+        robotArm.drop()
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()

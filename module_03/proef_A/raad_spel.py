3e41bb071f0876081c07c35209a5ea3914cbebfc
Seantheman01 <sean.v.g@outlook.com>
2022-12-01 09:54:50 +0100

    committed

--- module_03/proef_A/raad_spel.py
+++ module_03/proef_A/raad_spel.py
@@ -1,3 +1,14 @@
+punten = 0
 import random
-a = random.randint(1,1000)
-getal = int(input("Noem een getal: "))
+getal = random.randint(1,1000)
+print(getal)
+for x in range(10):
+    getal_typen = int(input("Typ een getal: "))
+    if getal_typen != getal:
+        if getal_typen < getal:
+            print("hoger")
+        elif getal_typen > getal:
+            print("lager")
+    elif getal_typen == getal:
+        print("Dat is het getal!")
+        input("Nog een ronde? ")

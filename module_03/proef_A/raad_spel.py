f65a074020227906b72d71c0ed31a9c4b8da204d
Seantheman01 <sean.v.g@outlook.com>
2022-12-01 12:19:25 +0100

    committed

--- module_03/proef_A/raad_spel.py
+++ module_03/proef_A/raad_spel.py
@@ -1,26 +1,20 @@
 punten = 0
 import random
 getal = random.randint(1,1000)
-
-begin = input("Wil je beginnen? ")
-if begin == 'nee':
-    print("O, jammer...")
-    print(f"punten: {punten}")
-elif begin == 'ja':
-    for x in range(10):
-        getal_typen = int(input("Typ een getal: "))
-        if getal_typen != getal:
-            if getal_typen < getal:
-                print("hoger")
-            elif getal_typen > getal:
-                print("lager")
-        elif getal_typen == getal:
-            print("Geraden!")
-            punten += 1
+for x in range(10):
+    getal_typen = int(input("Typ een getal: "))
+    if getal_typen != getal:
+        if getal_typen < getal:
+            print("hoger")
+        elif getal_typen > getal:
+            print("lager")
+    elif getal_typen == getal:
+        print("Geraden!")
+        punten += 1
+        print(f"punten: {punten}")
+        verder = input("Nog een ronde? ")
+        if verder == 'nee':
             print(f"punten: {punten}")
-            verder = input("Nog een ronde? ")
-            if verder == 'nee':
-                print(f"punten: {punten}")
-                break
-            elif verder != 'ja' and verder != 'nee':
-                input("Kies ja of nee: ")
+            break
+        elif verder != 'ja' and verder != 'nee':
+            input("Kies ja of nee: ")

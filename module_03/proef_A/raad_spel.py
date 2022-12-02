e8f7bcb78783a06a7fb69b4231495ba9e7dd9742
Seantheman01 <sean.v.g@outlook.com>
2022-12-02 09:47:40 +0100

    committed

--- module_03/proef_A/raad_spel.py
+++ module_03/proef_A/raad_spel.py
@@ -2,33 +2,41 @@ import random
 
 
 getal = random.randint(1,1000)
+print(getal)
 punten = 0
+verschil = 0
 RONDES = 20
+POGINGEN = 10
 
 begin = input("Wil je beginnen? ")
-for x in range(20):
+for x in range(RONDES):
     if begin == 'nee':
         break
+    
     elif begin == 'ja':
-        for y in range(10):
+        for y in range(POGINGEN):
             getal_typen = int(input("Typ een getal: "))
             if getal_typen != getal:
+                
                 if getal_typen < getal:
                     print("hoger")
                     if abs(50):
                         print("Je bent warm! ")
                     elif abs(20):
                         print("Je bent heel warm! ")
+                        
                 elif getal_typen > getal:
                     print("lager")
-                    if abs(50):
+                    if (50):
                         print("Je bent warm! ")
                     elif abs(20):
                         print("Je bent heel warm! ")
+                        
                 elif getal_typen >= 1000:
                     input("Dat is te hoog! ")
                 elif getal_typen <= 1:
                     input("Dat is te laag! ")
+                    
             elif getal_typen == getal:
                 print("Geraden!")
                 punten += 1
@@ -42,3 +50,5 @@ for x in range(20):
     else:
         input("Typ ja of nee: ")
 print(f"{punten}")
+if RONDES == 20:
+    print("Klaar")

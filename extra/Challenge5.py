ef2a0b33be773422b7d7703a5ade327d27f2e17f
Seantheman01 <sean.v.g@outlook.com>
2022-12-02 13:40:19 +0100

    commit

--- extra/Challenge5.py
+++ extra/Challenge5.py
@@ -1,5 +1,4 @@
-lijst = ["anton", "berat", "cors", "dirk", "eduard"]
-leeftijd = [16, 17, 12, 29, 15]
-
-for naam in lijst:
-    print("Hallo", naam)
+lijst1 = ["Sean", "Berat", "Umut", "Noa", "Tobias"]
+lijst2 = ["16", "17", "15", "18", "15"]
+for naam, leeftijd in zip(lijst1, lijst2):
+    print("Hallo", naam, ". Je bent", leeftijd, "jaar oud.")

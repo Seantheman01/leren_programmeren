b769b5fa0176a882ee5097990287360c9ff92c26
Seantheman01 <sean.v.g@outlook.com>
2022-12-02 10:13:13 +0100

    committed

--- module_03/proef_A/raad_spel.py
+++ module_03/proef_A/raad_spel.py
@@ -1,36 +1,35 @@
 import random
 
 
-getal = random.randint(1,1000)
-print(getal)
 punten = 0
 verschil = 0
-RONDES = 20
-POGINGEN = 10
 
 begin = input("Wil je beginnen? ")
-for x in range(RONDES):
+for x in range(20):
     if begin == 'nee':
+        print("Oh, jammer...")
         break
-    
     elif begin == 'ja':
-        for y in range(POGINGEN):
+        for y in range(10):
+            getal = random.randint(1,1000)
+            print(getal)
             getal_typen = int(input("Typ een getal: "))
+            verschil = abs(getal_typen - getal)
             
             if getal_typen != getal:
                 if getal_typen < getal:
                     print("hoger")
-                    if abs(getal_typen - 50):
-                        print("Je bent warm! ")
-                    elif abs(20):
+                    if verschil <= 20:
                         print("Je bent heel warm! ")
+                    elif verschil <= 50:
+                        print("Je bent warm! ")
                         
                 elif getal_typen > getal:
                     print("lager")
-                    if (50):
-                        print("Je bent warm! ")
-                    elif abs(20):
+                    if verschil <= 20:
                         print("Je bent heel warm! ")
+                    elif verschil <= 50:
+                        print("Je bent warm! ")
                         
                 elif getal_typen >= 1000:
                     input("Dat is te hoog! ")
@@ -41,14 +40,19 @@ for x in range(RONDES):
                 print("Geraden!")
                 punten += 1
                 print(f"punten: {punten}")
+                
                 verder = input("Nog een ronde? ")
                 if verder == 'nee':
-                    print(f"punten: {punten}")
+                    print(f"Totale punten: {punten}")
                     quit()
                 elif verder != 'ja' and verder != 'nee':
                     input("Kies ja of nee: ")
+                    
+            else:
+                input("Typ een getal in: ")
     else:
         input("Typ ja of nee: ")
 print(f"{punten}")
-if RONDES == 20:
+
+if x == 20:
     print("Klaar")

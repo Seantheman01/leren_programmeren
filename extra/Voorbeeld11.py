f4fc314ed1f2ab798797bd75912ede6bb9e3a73f
Seantheman01 <sean.v.g@outlook.com>
2022-12-05 09:32:18 +0100

    commit

---
+++ extra/Voorbeeld11.py
@@ -0,0 +1,13 @@
+mijn_dict = {
+    123456789: "Jouke Corbijn",
+    123456788: "Jan Janssen",
+    123456787: "Ogulcan Dinc",
+    123456786: "Renson Kooij"  
+}
+print(mijn_dict)
+
+for val in mijn_dict.values():
+    print(val) # hiermee print die de values
+    
+for ke in mijn_dict.keys():
+    print(ke) # hiermee print die de keys

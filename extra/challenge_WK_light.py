3fd0cd01816944b84c702efa856f3c3cc0806824
Seantheman01 <sean.v.g@outlook.com>
2022-12-08 09:42:38 +0100

    committed

--- extra/challenge_WK_light.py
+++ extra/challenge_WK_light.py
@@ -2,3 +2,15 @@ land1 = input("Noem het eerste land dat speelt in groep A: ")
 land2 = input("Noem het tweede land dat speelt in groep A: ")
 land3 = input("Noem het derde land dat speelt in groep A: ")
 
+print(f"""Wedstrijd |'thuis'|'uit'| Doelpunten land 1 | Doelpunten land 2 | Winnaar
+1         | {land1}    | {land2}  | 2                 | 0                 | {land1}
+2         | {land2}    | {land3}  | 1                 | 2                 | {land3}
+3         | {land1}    | {land3}  | 3                 | 1                 | {land1} """)
+
+
+print(f"""Wedstrijd {land1} - {land2} eindigde in: 2 - 0
+Overzicht groep 
+Land | Doelsalo (gemaakt - tegen) | Puntentotaal
+{land1}   | 2 - {land2}                     | 3
+{land2}   | 0 - {land1}                     | -3
+{land3}   | 0                          | 0 """)

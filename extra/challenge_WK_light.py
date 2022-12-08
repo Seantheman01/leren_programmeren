dc9a8e7cbf3b547e53a545c1677c878e9c91b285
Seantheman01 <sean.v.g@outlook.com>
2022-12-08 09:11:49 +0100

    commit

---
+++ extra/challenge_WK_light.py
@@ -0,0 +1,4 @@
+land1 = input("Noem het eerste land dat speelt in groep A: ")
+land2 = input("Noem het tweede land dat speelt in groep A: ")
+land3 = input("Noem het derde land dat speelt in groep A: ")
+

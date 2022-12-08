806af3e210d52993311c4b1ce7d81922f21cf30a
Seantheman01 <sean.v.g@outlook.com>
2022-12-08 14:06:55 +0100

    committed

--- module_04/deel_01/dagen.py
+++ module_04/deel_01/dagen.py
@@ -1,2 +1,35 @@
-dagen = ("maandag", "dinsdag", "woensdaqg", "donderdag", "vrijdag", "zaterdag", "zondag")
-print(dagen)
+dagen = ("maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag")
+
+print("Alle dagen van de week: ")
+for x in dagen:
+    print(x)
+
+print("")
+
+print("Werkdagen: ")
+for y in range(5):
+    print(dagen[y])
+
+print("")
+
+print("Weekenddagen: ")
+for z in range(5,7):
+    print(dagen[z])
+    
+print("")    
+    
+print("Alle dagen omgekeerd: ")
+for a in range(6,-1,-1):
+    print(dagen[a])
+
+print("")
+
+print("Werkdagen omgekeerd: ")
+for b in range(4,-1,-1):
+    print(dagen[b])
+    
+print("")
+    
+print("Weekenddagen omgekeerd: ")
+for c in range(6,4,-1):
+    print(dagen[c])

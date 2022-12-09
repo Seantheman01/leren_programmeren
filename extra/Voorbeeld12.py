120409cff81e302a48885910c53a4e8c314b85fb
Seantheman01 <sean.v.g@outlook.com>
2022-12-09 13:24:26 +0100

    committed

--- extra/Voorbeeld12.py
+++ extra/Voorbeeld12.py
@@ -9,7 +9,12 @@ while True:
         if input("Wil je bijwerken? Typ ja of nee: ") != 'ja':
             continue
 
-leeftijd = int(input("Wat is je leeftijd? "))
+    leeftijd = int(input("Wat is je leeftijd? "))
+    
+    if leeftijd in namen_lijst.values():
+        print(f"Er zit al iemand in die {leeftijd} jaar oud is.")
 
-namen_lijst[naam] = leeftijd
+    namen_lijst[naam] = leeftijd
+    # namen_lijst.update({naam : leeftijd}) zo kan het ook
+
 print(namen_lijst)

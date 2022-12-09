9e152321b05255768de422731b0f0407fd00aab4
Seantheman01 <sean.v.g@outlook.com>
2022-12-09 09:26:25 +0100

    committed

--- module_04/M&M's.py
+++ module_04/M&M's.py
@@ -1,2 +1,5 @@
-M_en_M = ("oranje", "blauw", "groen", "bruin")
+import random
 
+M_en_M = ("oranje", "blauw", "groen", "bruin")
+aantal = int(input("Hoeveel M&M's moeten er aan de zak toegevoegd worden? "))
+inhoud = []

a0017ac46e5fb0c8ff4124ec37f7053c3ac3a7ad
Seantheman01 <sean.v.g@outlook.com>
2022-12-09 09:52:58 +0100

    committed

--- module_04/M&M's.py
+++ module_04/M&M's.py
@@ -1,5 +1,24 @@
 import random
 
-M_en_M = ("oranje", "blauw", "groen", "bruin")
+kleuren = ("oranje", "blauw", "groen", "bruin")
 aantal = int(input("Hoeveel M&M's moeten er aan de zak toegevoegd worden? "))
 inhoud = []
+
+for x in range(aantal):
+    inhoud.append(random.choice[kleuren])
+    
+print("Oranje: ")
+for x in kleuren:
+    print(x)
+    
+print("Groen: ")
+for y in kleuren:
+    print(y)
+    
+print("Blauw: ")
+for z in kleuren:
+    print(z)
+    
+print("Bruin: ")
+for a in kleuren:
+    print(a)

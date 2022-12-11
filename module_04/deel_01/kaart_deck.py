037cc85d8f2b3330f8db758a6fd3c92946ca88c2
Seantheman01 <sean.v.g@outlook.com>
2022-12-11 16:15:30 +0100

    committed

--- module_04/deel_01/kaart_deck.py
+++ module_04/deel_01/kaart_deck.py
@@ -1,10 +1,13 @@
 import random
 
-vormen = ['harten', 'klaveren', 'schoppen', 'ruiten']
-soorten = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'boer', 'vrouw','heer', 'aas', 'joker', 'joker']
+vormen = ('harten', 'klaveren', 'schoppen', 'ruiten')
+soorten = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'boer', 'vrouw','heer', 'aas', 'joker', 'joker')
+kaarten = []
 
-for z in range(soorten):
-    print(f"""kaart 1:
+for x in soorten:
+    random_vorm = random.choice(vormen)
+    kaarten.append(random_vorm)
+print(f"""kaart 1:
 kaart 2:
 kaart 3:
 kaart 4:
@@ -12,4 +15,4 @@ kaart 5:
 kaart 6:
 kaart 7:
 
-deck (47 kaarten): {soorten}""")
+deck (47 kaarten): {kaarten}""")

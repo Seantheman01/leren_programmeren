3932dc4f24701e4af5e46b1d121322b9e19fc49a
Seantheman01 <sean.v.g@outlook.com>
2022-12-11 15:58:38 +0100

    committed

--- module_04/deel_01/kaart_deck.py
+++ module_04/deel_01/kaart_deck.py
@@ -2,3 +2,14 @@ import random
 
 vormen = ['harten', 'klaveren', 'schoppen', 'ruiten']
 soorten = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'boer', 'vrouw','heer', 'aas', 'joker', 'joker']
+
+for z in range(soorten):
+    print(f"""kaart 1:
+kaart 2:
+kaart 3:
+kaart 4:
+kaart 5:
+kaart 6:
+kaart 7:
+
+deck (47 kaarten): {soorten}""")

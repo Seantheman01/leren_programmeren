01293c15f2c0f702cb9a151636cfc5183cccfe03
Seantheman01 <sean.v.g@outlook.com>
2022-12-12 12:45:10 +0100

    commit

---
+++ module_04/deel_01/M&M's dictionary.py
@@ -0,0 +1,11 @@
+import random
+
+kleuren = ("oranje", "blauw", "groen", "bruin")
+aantal = int(input("Hoeveel M&M's moeten er aan de zak toegevoegd worden? "))
+inhoud = []
+
+for x in range(aantal):
+    random_kleur = random.choice(kleuren)
+    inhoud.append(random_kleur)
+    
+print(inhoud)

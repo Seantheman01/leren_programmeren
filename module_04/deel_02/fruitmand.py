52fa1113e90c1b8a35a02e48d6c549026d3e3381
Seantheman01 <sean.v.g@outlook.com>
2022-12-12 13:03:16 +0100

    committed

--- module_04/deel_02/fruitmand.py
+++ module_04/deel_02/fruitmand.py
@@ -1,6 +1,11 @@
 from fruitmand import fruitmand
 
-appel_dict = fruitmand[1]
-print(type(appel_dict))
-print(appel_dict)
-print(appel_dict["name"])
+# appel_dict = fruitmand[1]
+# print(type(appel_dict))
+# print(appel_dict)
+# print(appel_dict["name"])
+
+for fruit in fruitmand:
+    print(type(fruit))
+    print(fruit['round'])
+    print(fruit)

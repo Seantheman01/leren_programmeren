a45fa00922ea52bc0a286117135095e827c6ef93
Seantheman01 <sean.v.g@outlook.com>
2022-12-14 22:12:03 +0100

    committed

--- module_04/deel_01/M&M's dictionary.py
+++ module_04/deel_01/M&M's dictionary.py
@@ -2,16 +2,13 @@ import random
 
 kleuren = ["rood", "blauw", "groen", "geel", "bruin"]
 aantal = int(input("Hoeveel M&M's moeten er aan de zak toegevoegd worden? "))
-inhoud = []
+inhoud = {}
 
+for x in kleuren:
+    inhoud[x] = 0
+    
 for x in range(aantal):
-    zak = {
-        'rood' : 1,
-        'blauw' : 1,
-        'groen' : 1,
-        'geel' : 1,
-        'bruin' : 1
-    }
+    inhoud[random.choice(kleuren)] += random.randint(0,2)
     
 
-print(f"Je hebt {zak['rood']} rode, {zak['blauw']} blauwe, {zak['groen']} groene, {zak['geel']} gele, en {zak['bruin']} bruine M&M's.")
+print(inhoud)

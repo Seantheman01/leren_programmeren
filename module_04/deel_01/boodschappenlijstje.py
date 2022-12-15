50c9dcb205c1c8d3a83c57f3b8918357ef3c80d2
Seantheman01 <sean.v.g@outlook.com>
2022-12-15 09:13:47 +0100

    committed

---
+++ module_04/deel_01/boodschappenlijstje.py
@@ -0,0 +1,7 @@
+for x in range(7):
+    boodschappen = input("Welke boodschappen wil je? ")
+    hoeveelheid = input("Hoeveel wil je daarvan hebben? ")
+
+print("-[BOODSCHAPPENLIJST]-")
+print()
+print("----------------------")

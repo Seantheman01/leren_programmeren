08c3a33065656bd5ab484a33b1d6f2b89784bdbc
Seantheman01 <sean.v.g@outlook.com>
2022-12-15 10:20:51 +0100

    committed

--- module_04/deel_01/boodschappenlijstje.py
+++ module_04/deel_01/boodschappenlijstje.py
@@ -1,8 +1,20 @@
-for x in range():
-    boodschappen = input("Welke boodschappen heb je nodig? ")
-    hoeveelheid = int(input("Hoeveel heb je daarvan nodig? "))
-    verder = input("Heb je meer spullen nodig? ")
+lijstje = {}
 
-print("-[ BOODSCHAPPENLIJST ]-")
-print()
-print("-----------------------")
+while True:
+    boodschappen = input("Welke boodschappen heb je nodig? ")
+    if boodschappen == '':
+        input("Vul een product in: ")
+    hoeveelheid = int(input("Hoeveel heb je daarvan nodig? "))
+    if hoeveelheid == '':
+        int(input("Vul een getal in: "))
+    verder = input("Heb je meer spullen nodig? ")
+    
+    if verder == 'nee':
+        break
+    elif verder != 'ja' and verder != 'nee':
+        input("Vul ja of nee in: ")
+    
+for x in boodschappen:
+    print("-[ BOODSCHAPPENLIJST ]-")
+    print(f"{x}x {lijstje}")        
+    print("-----------------------")

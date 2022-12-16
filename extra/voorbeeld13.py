031561a8c94056cd58514f802be84bf6ede678dc
Seantheman01 <sean.v.g@outlook.com>
2022-12-16 09:50:55 +0100

    committed

--- extra/voorbeeld13.py
+++ extra/voorbeeld13.py
@@ -4,9 +4,11 @@ def vraag_getal(vraag: str) -> int:
         try:
             getal = int(input(vraag))
             break
+        
         except ValueError:
             print("Je moet wel een getal invullen: ")
             continue   
+        
     return getal
 
 leeftijd = vraag_getal("Voer uw leeftijd in: ")

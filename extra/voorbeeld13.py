d69dd1ef3e720acaa2fc3f40850bc2ebf8a248e5
Seantheman01 <sean.v.g@outlook.com>
2022-12-16 10:12:01 +0100

    committed

--- extra/voorbeeld13.py
+++ extra/voorbeeld13.py
@@ -15,3 +15,5 @@ leeftijd = vraag_getal("Voer uw leeftijd in: ")
 geboorte_jaar = vraag_getal("Voer uw geboortejaar in: ")
 geboorte_maand = vraag_getal("Voer uw geboortemaand in: ")
 geboorte_dag = vraag_getal("Voer uw geboortedag in: ")
+
+print(leeftijd)

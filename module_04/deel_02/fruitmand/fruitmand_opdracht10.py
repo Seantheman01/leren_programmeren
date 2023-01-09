952dc5ff2c19cc201bb01dbfc32d5ace0e7c2733
Seantheman01 <sean.v.g@outlook.com>
2023-01-09 10:48:11 +0100

    committed

--- module_04/deel_02/fruitmand/fruitmand_opdracht10.py
+++ module_04/deel_02/fruitmand/fruitmand_opdracht10.py
@@ -2,11 +2,8 @@ from fruitmand import fruitmand
 from operator import itemgetter
 
 gewichten = sorted(fruitmand, key=itemgetter('weight'))
+
+for x in fruitmand:
+    gewichten.append(x['weight'])
+
 print(gewichten)
-
-# gewichten = []
-
-# for x in fruitmand:
-#     gewichten.append(x['weight'])
-
-# print(sorted(gewichten)

42ee9e5bcee6a086c06adefe7e9624f35b431968
Seantheman01 <sean.v.g@outlook.com>
2023-01-09 13:17:40 +0100

    committed

--- extra/voorbeeld14.py
+++ extra/voorbeeld14.py
@@ -1,6 +1,7 @@
 def vraag_letter(vraag: str) -> str:
     while True:
         letter = input(vraag)
+        
         if len(letter) >1:
             print("Je moet wel een letter invoeren! ")
         else:
